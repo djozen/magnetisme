bfcc6ca2b3655ac06eb15c15ee5067abc105969a
Johan Nagou <djozen.web@gmail.com>
2025-12-04 11:17:01 +0100

    feat: Add host configuration to Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3000,
-    open: true
+    open: true,
+    host: '127.0.0.1'
   },
   build: {
     rollupOptions: {
